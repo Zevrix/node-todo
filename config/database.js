03179928f8c7ac95d39a401693f0d20edbfcb92d
Arash Rai <arashrai17@gmail.com>
2016-08-07 20:50:39 -0400

    dasdaw

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://user:123@ec2-52-206-234-241.compute-1.amazonaws.com:27017/dummyDB'
+    localUrl: 'mongodb://user:123@arashrai.com:27017/dummyDB'
 };
